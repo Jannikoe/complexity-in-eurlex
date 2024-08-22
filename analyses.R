9b2750d41ce471cdd1f84c799b75871c78919b61
Jannik Köhler <168744616+Jannikoe@users.noreply.github.com>
2024-08-22 11:53:25 +0200

    Add files via upload

--- analyses.R
+++ analyses.R
@@ -1,5 +1,5 @@
 #small function for descriptive statistics
-x <- '952'
+x <- '491'
 mean(red_german3[,x])
 sd(red_german3[,x])
 mean(red_english3[,x])
@@ -8,7 +8,7 @@ median(red_english3[,x])
 median(red_german3[,x])
 
 #small function for feature distributions
-Feature <- '459'
+Feature <- '491'
 Featurename <- "SUBTLEX Word Frequency/Million (N Type)" 
 BOXPLOT <- data.frame(y=c(red_german3_z[,Feature],red_english3_z[,Feature]),
                       x=c(rep("Deutsch",length(red_german3_z[,Feature])),
